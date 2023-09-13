615bd1db7918fc3d7add08f3405072a1805d10f2
dvxiaofan <1353815633@qq.com>
2023-09-13 10:09:03 +0800

    login01

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Body, Post, BadRequestException} from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserDto } from './user.dto';
+
+const users = [
+  { username: 'ccfan', password: 'ccfan666', email: 'ccfan@666.com' },
+  { username: 'ccyou', password: 'ccyou999', email: 'ccyou@999.com' },
+]
 
 @Controller()
 export class AppController {
@@ -8,5 +14,27 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('login')
+  login(@Body() userDto: UserDto) {
+    const user = users.find(item => item.username === userDto.username);
+
+    if (!user) {
+      throw new BadRequestException('user not found');
+    }
+
+    if (user.password !== userDto.password) {
+      throw new BadRequestException('password error');
+    }
+
+    return {
+      userInfo: {
+        username: user.username,
+        email: user.email,
+      },
+      accessToken: '1234567890',
+      refreshToken: '0987654321',
+    }
   }
 }
