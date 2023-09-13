fc47a2a9a877df262cc91919c5a51190a27ea89d
dvxiaofan <1353815633@qq.com>
2023-09-13 10:22:08 +0800

    login01

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,28 @@
-import {Controller, Get, Body, Post, BadRequestException} from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Body,
+  Post,
+  BadRequestException,
+  Inject
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { UserDto } from './user.dto';
+import {JwtService} from "@nestjs/jwt";
 
 const users = [
   { username: 'ccfan', password: 'ccfan666', email: 'ccfan@666.com' },
   { username: 'ccyou', password: 'ccyou999', email: 'ccyou@999.com' },
+  { username: 'guang', password: '666666', email: 'guang@999.com' },
+
 ]
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Inject(JwtService)
+  private jwtService: JwtService;
 
   @Get()
   getHello(): string {
@@ -28,13 +41,26 @@ export class AppController {
       throw new BadRequestException('password error');
     }
 
+    const accessToken = this.jwtService.sign({
+        username: user.username,
+      email: user.email,
+    }, {
+      expiresIn: '0.5h'
+    });
+
+    const refreshToken = this.jwtService.sign({
+        username: user.username,
+    }, {
+        expiresIn: '7d'
+    });
+
     return {
       userInfo: {
         username: user.username,
         email: user.email,
       },
-      accessToken: '1234567890',
-      refreshToken: '0987654321',
+      accessToken,
+      refreshToken,
     }
   }
 }
